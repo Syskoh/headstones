9ccad5a1ce70d60df915eaab461b8c9d452d9858
Syskoh <syskoh1998@gmail.com>
2025-02-28 17:35:27 +0100

    Removed unused import

--- src/main/java/tk/alex3025/headstones/utils/WorldGuardHook.java
+++ src/main/java/tk/alex3025/headstones/utils/WorldGuardHook.java
@@ -5,7 +5,6 @@ import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.StateFlag;
-import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
 import org.bukkit.entity.Player;
